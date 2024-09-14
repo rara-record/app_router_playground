968568e12947775e2b6f40cf735d3579008d521c
BORA <a52447879@gmail.com>
2024-09-14 15:54:56 +0900

    feat: streaming - edge runtime

---
+++ app/streaming/edge/product/[id]/page.tsx
@@ -0,0 +1,47 @@
+import {
+  RecommendedProducts,
+  RecommendedProductsSkeleton,
+} from '#/app/streaming/_component/recommended-products';
+import { SingleProduct } from '#/app/streaming/_component/single-product';
+import { Ping } from '#/ui/ping';
+import { Suspense } from 'react';
+
+export const runtime = 'experimental-edge';
+
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div className="space-y-8 lg:space-y-14">
+      <SingleProduct
+        data={fetch(
+          `https://app-playground-api.vercel.app/api/products?id=${params.id}`
+        )}
+      />
+
+      <div className="relative">
+        <div className="absolute -left-4 top-2">
+          <Ping />
+        </div>
+      </div>
+
+      <Suspense fallback={<RecommendedProductsSkeleton />}>
+        <RecommendedProducts
+          path="/streaming/node/product"
+          data={fetch(
+            // We intentionally delay the response to simulate a slow data
+            // request that would benefit from streaming
+            `https://app-playground-api.vercel.app/api/products?delay=500&filter=${params.id}`,
+            {
+              // We intentionally disable Next.js Cache to better demo
+              // streaming
+              cache: 'no-store',
+            }
+          )}
+        />
+      </Suspense>
+    </div>
+  );
+}
